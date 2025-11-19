199d9674d12c7e9c84800d9a4f69d0dfb90b8a67
Adamrashid781 <adamrashid781@gmail.com>
2025-11-18 20:33:59 -0800

    Added events database schema
    still needs testing

--- Mern-Stack/backend/server.js
+++ Mern-Stack/backend/server.js
@@ -12,6 +12,34 @@ const app = express();
 app.get("/products", (req, res) => {
     res.send("Server is ready");
 });
+
+
+// code to post the event data to the database
+import Event from "./models/Events.model.js";
+
+// testing code for Postman app.
+app.use(express.json()); // allows us to accept JSON data in req.body below
+app.post("/api/events", async (req, res) =>{
+    const event = req.body; // ← user will send this data to DB
+
+    if(!event.eventName || !event.organizer || !event.date || !event.time || !event.details ){
+        return res.status(400).json({ success:false, message: "All fields are required" });
+    }
+
+    const newEvent = new Event(event);
+
+    try{
+        await newEvent.save();
+        res.status(201).json({ success:true, message: "Event created successfully", data: newEvent });
+    }
+    catch(error){
+        console.error("Error in creating event:", error.message);
+        res.status(500).json({ success:false, message: "Server Error" });
+    }
+});
+
+
+
 app.listen(5001, () => {
     console.log("Server started at http://localhost:5001");
 });
