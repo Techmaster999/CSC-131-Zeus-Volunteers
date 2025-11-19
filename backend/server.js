00200958943c8cc77b3330c6d22b8a952a6f4161
Techmaster999 <piercetech9@gmail.com>
2025-11-18 17:14:52 -0800

    Hotfix 2

--- backend/server.js
+++ backend/server.js
@@ -14,17 +14,17 @@ app.use(express.json());
 app.use(cors());
 
 //Import Routes
-import authRoutes from "./routes/auth.js";
-import particpationRoutes from "./routes/participation.js";
+//import authRoutes from "./routes/auth.js";
+//import particpationRoutes from "./routes/participation.js";
 
 //Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/participation", particpationRoutes);
+//app.use("/api/auth", authRoutes);
+//app.use("/api/participation", particpationRoutes);
 
 //test route
-app.get("/products",(req, res) => { }); {
-res.send("Server is ready");
-};
+//app.get("/products",(req, res) => { }); {
+//res.send("Server is ready");
+//};
 
 //
 app.listen(5000, () => {
