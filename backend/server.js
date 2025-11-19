7033ec557fbc32114d84fc78d90066a3b7d6a9f2
Techmaster999 <piercetech9@gmail.com>
2025-11-18 16:58:12 -0800

    Hotfix for the server

--- backend/server.js
+++ backend/server.js
@@ -14,19 +14,19 @@ app.use(express.json());
 app.use(cors());
 
 //Import Routes
-import authRoutes from "./routes/auth.js";
-import particpationRoutes from "./routes/participation.js";
+// import authRoutes from "./routes/auth.js";
+// import particpationRoutes from "./routes/participation.js";
 
 //Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/participation", particpationRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/participation", particpationRoutes);
 
 //test route
-app.get("/products",(req, res) => { }); {
-res.send("Server is ready");
-};
+//app.get("/products",(req, res) => { }); {
+//res.send("Server is ready");
+//};
 
 //
-app.listen(5000, () => {
-console.log("Server started at http://localhost:5000");
+app.listen(5001, () => {
+console.log("Server started at http://localhost:5001");
 });
