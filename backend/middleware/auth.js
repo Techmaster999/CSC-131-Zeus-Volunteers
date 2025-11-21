73cfacb0f4565fe1ce5f191b0076b816ae50ef0c
Adamrashid781 <adamrashid781@gmail.com>
2025-11-20 20:07:06 -0800

    created and tested user account creation page

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/user.js'
+import User from '../models/Accounts.model.js';
 
 //protect routes - verify JWT token
 export const protect = async (requestAnimationFrame, resizeBy, next) => {
